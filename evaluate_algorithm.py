e789ebc7e5904a1ef7ddf268e368d9ed1cb72162
amana-newzera <amana@newzera.com>
2021-04-15 00:21:48 +0530

    resolved segmentation fault issue and added some code to improve performance

--- evaluate_algorithm.py
+++ evaluate_algorithm.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn import metrics
 import numpy as np
-from scipy.misc import comb
+from scipy.special import comb
 import glob
 import os
 
@@ -43,9 +43,16 @@ def precision_recall_fmeasure(cooccurrence_matrix):
 
 def run(input_dir, output_dir):
 
-    original_clusters_path = input_dir
+    print("---------------holaa--------------------------")
+    original_clusters_path = input_dir#remove_redundancy_chains
+    print(original_clusters_path)
+
+    path = os.path.join(original_clusters_path,"ground_truth_chains")
+
     file_name = '*.csv'
-    all_files = glob.glob(os.path.join(original_clusters_path, file_name))
+    all_files = glob.glob(os.path.join(path, file_name))#storing all input files
+
+    print(all_files)
 
     gkg_id_to_index = {}
     class_labels_dict = {}
@@ -53,14 +60,15 @@ def run(input_dir, output_dir):
     index = 0
 
     for f in all_files:
-        df = pd.read_csv(f, header=None, encoding='latin-1')
-        df_list = df.values.tolist()
+        df = pd.read_csv(f, header=None, encoding='latin-1')#storing the input in df
+        df_list = df.values.tolist()#stacking all dfs together in a list
 
         for row in df_list:
             try:
                 gkg_id = row[0].strip()
             except AttributeError:
                 continue
+            print(gkg_id)
             class_labels_dict[gkg_id] = label
             gkg_id_to_index[gkg_id] = index
             index += 1
@@ -73,7 +81,8 @@ def run(input_dir, output_dir):
 
     formed_clusters_path = output_dir
     file_name = '*.csv'
-    all_files = glob.glob(os.path.join(formed_clusters_path, file_name))
+    all_files = glob.glob(os.path.join(formed_clusters_path, file_name))#storing output files..
+
 
     cluster_labels_dict = {}
     label = 1
@@ -82,14 +91,32 @@ def run(input_dir, output_dir):
         df_list = df.values.tolist()
 
         for row in df_list:
-            gkg_id = row[0]
+            gkg_id = row[0].strip()
+            print(gkg_id)
             cluster_labels_dict[gkg_id] = label
 
         label += 1
 
     cluster_labels = [0] * len(cluster_labels_dict)
     for key, value in cluster_labels_dict.items():
-        cluster_labels[gkg_id_to_index[key]] = value
+        if key in gkg_id_to_index:
+            cluster_labels[gkg_id_to_index[key]] = value
+
+    #maxn=max(len(class_labels_dict),len(cluster_labels_dict))
+
+    #class_labels = [0]*maxn
+    #for key, value in class_labels_dict.items():
+    #    class_labels[gkg_id_to_index[key]] = value
+
+    #cluster_labels = [0] * maxn
+    #for key, value in cluster_labels_dict.items():
+    #    if key in gkg_id_to_index:#checking that id is present in input or not
+    #        #if gkg_id_to_index[key] in cluster_labels:
+    #        cluster_labels[gkg_id_to_index[key]] = value
+
+
+    print(len(class_labels))
+    print(len(cluster_labels))
 
     matrix = metrics.cluster.contingency_matrix(class_labels, cluster_labels)
     rand_index, precision, recall, f1 = precision_recall_fmeasure(matrix)
